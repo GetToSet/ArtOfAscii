9a617523478b8f0e69ce3170119b5ae9b6aac999
Bunny Wong <bunnnywong@gmail.com>
2020-02-10 00:19:42 +0800

    Project rename and basic structures.

--- AsciiFan/Chapters/AsciiFan.playgroundchapter/Pages/02-HowImagesComposed.playgroundpage/LiveView.swift
+++ AsciiFan/Chapters/AsciiFan.playgroundchapter/Pages/02-HowImagesComposed.playgroundpage/LiveView.swift
@@ -8,5 +8,4 @@
 import UIKit
 import PlaygroundSupport
 
-// Instantiate a new instance of the live view from the book's auxiliary sources and pass it to PlaygroundSupport.
 PlaygroundPage.current.liveView = instantiateLiveView()
