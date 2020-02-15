e34e1af265ffe9f8393b40adb555d876f0dccee4
Bunny Wong <bunnnywong@gmail.com>
2020-02-15 17:24:01 +0800

    Drawing histograms.

--- FanOfAscii/Modules/BookCore.playgroundmodule/Sources/UserInterface/Chapters/01-FanOfAscii/02-HowImagesComposed/Views/MagnifierView.swift
+++ FanOfAscii/Modules/BookCore.playgroundmodule/Sources/UserInterface/Chapters/01-FanOfAscii/02-HowImagesComposed/Views/MagnifierView.swift
@@ -46,31 +46,33 @@ class MagnifierView: UIView {
 
         let croppingRect = CGRect(origin: center, size: CGSize(width: samplePixels, height: samplePixels))
 
-        if let context = UIGraphicsGetCurrentContext() {
-            let pixelSize = Int(min(self.bounds.width / CGFloat(samplePixels), self.bounds.height / CGFloat(samplePixels)))
-
-            for y in 0..<samplePixels {
-                for x in 0..<samplePixels {
-                    let ix = x + Int(croppingRect.minX)
-                    let iy = y + Int(croppingRect.minY)
-                    let pixelColor: UIColor = pixelColorAt(x: ix, y: iy, rawData: rawData, size: image.size) ?? .black
-
-                    context.setFillColor(pixelColor.cgColor)
-                    context.fill(CGRect(x: x * pixelSize, y: y * pixelSize, width: pixelSize, height: pixelSize))
-                }
-            }
-
-            context.setStrokeColor(UIColor.lightGray.cgColor)
-            for i in 0..<samplePixels {
-                context.stroke(CGRect(x: i * pixelSize, y: 0, width: pixelSize, height: pixelSize * samplePixels))
-                context.stroke(CGRect(x: 0, y: i * pixelSize, width: pixelSize * samplePixels, height: pixelSize))
-            }
-
-            let mid = samplePixels / 2
-            context.setStrokeColor(UIColor.white.cgColor)
-            context.stroke(CGRect(x: mid * pixelSize, y: 0, width: pixelSize, height: pixelSize * samplePixels))
-            context.stroke(CGRect(x: 0, y: mid * pixelSize, width: pixelSize * samplePixels, height: pixelSize))
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
         }
+
+        let pixelSize = Int(min(self.bounds.width / CGFloat(samplePixels), self.bounds.height / CGFloat(samplePixels)))
+
+        for y in 0..<samplePixels {
+            for x in 0..<samplePixels {
+                let ix = x + Int(croppingRect.minX)
+                let iy = y + Int(croppingRect.minY)
+                let pixelColor: UIColor = pixelColorAt(x: ix, y: iy, rawData: rawData, size: image.size) ?? .black
+
+                context.setFillColor(pixelColor.cgColor)
+                context.fill(CGRect(x: x * pixelSize, y: y * pixelSize, width: pixelSize, height: pixelSize))
+            }
+        }
+
+        context.setStrokeColor(UIColor.lightGray.cgColor)
+        for i in 0..<samplePixels {
+            context.stroke(CGRect(x: i * pixelSize, y: 0, width: pixelSize, height: pixelSize * samplePixels))
+            context.stroke(CGRect(x: 0, y: i * pixelSize, width: pixelSize * samplePixels, height: pixelSize))
+        }
+
+        let mid = samplePixels / 2
+        context.setStrokeColor(UIColor.white.cgColor)
+        context.stroke(CGRect(x: mid * pixelSize, y: 0, width: pixelSize, height: pixelSize * samplePixels))
+        context.stroke(CGRect(x: 0, y: mid * pixelSize, width: pixelSize * samplePixels, height: pixelSize))
     }
 
     func pixelColorAt(x: Int, y: Int, rawData: CFData, size: CGSize) -> UIColor? {
--- FanOfAscii/Modules/BookCore.playgroundmodule/Sources/UserInterface/Chapters/01-FanOfAscii/03-GrayscaleHistogramEqualization/Views/HistogramView.swift
+++ FanOfAscii/Modules/BookCore.playgroundmodule/Sources/UserInterface/Chapters/01-FanOfAscii/03-GrayscaleHistogramEqualization/Views/HistogramView.swift
@@ -13,11 +13,13 @@ class HistogramView: UIView {
         let green: [UInt]
         let blue: [UInt]
         let alpha: [UInt]
+        let pixelCount: UInt
     }
 
     var image: UIImage? {
         didSet {
             histogram = self.calculateHistogram()
+            setNeedsDisplay()
         }
     }
 
@@ -34,9 +36,56 @@ class HistogramView: UIView {
     }
 
     override func draw(_ rect: CGRect) {
-        // Draw rects with the histogram
-        
+        guard
+            let histogram = self.histogram,
+            let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+
+        let sampleCount = 256
+
+        var lumLevels = [CGFloat](repeating: 0.0, count: sampleCount)
+        for i in 0..<sampleCount {
+            lumLevels[i] = 0.2126 * CGFloat(histogram.red[i]) + 0.7152 * CGFloat(histogram.green[i]) + 0.0722 * CGFloat(histogram.blue[i])
+        }
+        let redLevels: [CGFloat] = histogram.red.map({ CGFloat($0) })
+        let greenLevels: [CGFloat] = histogram.green.map({ CGFloat($0) })
+        let blueLevels: [CGFloat] = histogram.blue.map({ CGFloat($0) })
+
+        drawHistogram(histogramVal: redLevels, color: .red, context: context)
+        drawHistogram(histogramVal: greenLevels, color: .green, context: context)
+        drawHistogram(histogramVal: blueLevels, color: .blue, context: context)
+        drawHistogram(histogramVal: lumLevels, color: .white, context: context)
     }
+
+    func drawHistogram(histogramVal: [CGFloat], color: UIColor, context: CGContext) {
+        let size = self.bounds.size
+
+        let sampleCount = histogramVal.count
+        let pixelPerSample = size.width / CGFloat(sampleCount - 1)
+
+        let levelMax = histogramVal.reduce(0.0) {
+            max($0, $1)
+        }
+        let yVals: [CGFloat] = histogramVal.map {
+            1.0 * size.height * (1.0 - CGFloat($0 / levelMax))
+        }
+
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        for i in 0..<sampleCount {
+            let plotPoint = CGPoint(x: CGFloat(i) * pixelPerSample, y: yVals[i])
+            print(yVals[i])
+            path.addLine(to: plotPoint)
+        }
+        color.setStroke()
+        context.setLineWidth(3.0)
+        path.stroke()
+    }
+
+}
+
+extension HistogramView {
 
     func calculateHistogram() -> Histogram? {
         guard let image = image,
@@ -46,14 +95,16 @@ class HistogramView: UIView {
             return nil
         }
 
-        let sourceHeight = Int(cgImage.height)
-        let sourceWidth = Int(cgImage.width)
+        let sourceHeight = UInt(cgImage.height)
+        let sourceWidth = UInt(cgImage.width)
         let sourceByteForRow = cgImage.bytesPerRow
-        
+
         var sourceBuffer = vImage_Buffer(data: sourceBitmapPointer,
-                                         height: UInt(sourceHeight),
-                                         width:  UInt(sourceWidth),
+                                         height: sourceHeight,
+                                         width: sourceWidth,
                                          rowBytes: sourceByteForRow)
+
+        let pixelCount: UInt = sourceHeight * sourceWidth
 
         var alpha = [UInt](repeating: 0, count: 256)
         var red = [UInt](repeating: 0, count: 256)
@@ -73,7 +124,7 @@ class HistogramView: UIView {
         if error != kvImageNoError {
             return nil
         }
-        return Histogram(red: red, green: green, blue: blue, alpha: alpha)
+        return Histogram(red: red, green: green, blue: blue, alpha: alpha, pixelCount: pixelCount)
     }
 
 }
