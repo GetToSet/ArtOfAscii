92ce2d254788e7d6e3520fecf769688c2322c106
Bunny Wong <bunnnywong@gmail.com>
2020-02-27 17:45:01 +0800

    Update license & acknowledgments.

--- ArtOfAscii/Chapters/02-Acknowledgements.playgroundchapter/Pages/01-Acknwoledgements.playgroundpage/LiveView.swift
+++ ArtOfAscii/Chapters/02-Acknowledgements.playgroundchapter/Pages/01-Acknwoledgements.playgroundpage/LiveView.swift
@@ -1,13 +1,8 @@
 //
-//  See LICENSE folder for this template’s licensing information.
-//
-//  Abstract:
-//  Instantiates a live view and passes it to the PlaygroundSupport framework.
+// Copyright © 2020 Bunny Wong
+// Created on 2019/12/18.
 //
 
 import UIKit
 import BookCore
 import PlaygroundSupport
-
-// Instantiate a new instance of the live view from BookCore and pass it to PlaygroundSupport.
-PlaygroundPage.current.liveView = instantiateLiveView()
--- ArtOfAscii/Chapters/02-Acknowledgements.playgroundchapter/Pages/01-Acknwoledgements.playgroundpage/main.swift
+++ ArtOfAscii/Chapters/02-Acknowledgements.playgroundchapter/Pages/01-Acknwoledgements.playgroundpage/main.swift
@@ -1,9 +1,76 @@
 //#-hidden-code
 //
-//  See LICENSE folder for this template’s licensing information.
+// Copyright © 2020 Bunny Wong
+// Created on 2019/12/18.
 //
-//  Abstract:
-//  The Swift file containing the source code edited by the user of this playground book.
-//
+
+// swiftlint:disable line_length
+
 //#-end-hidden-code
-let str = "Hello, playground"
+/*:
+# License
+
+Copyright © 2020 Bunny Wong
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+# Acknowledgements
+
+## Swift Playgrounds Author Template for Xcode 11.1
+
+Copyright © 2019 Apple Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## jQuery
+  
+Copyright JS Foundation and other contributors, https://js.foundation/
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+
+## Images
+
+### Sample Images
+
+* Photo by [Silas Baisch](https://unsplash.com/@silasbaisch) on [Unsplash](https://unsplash.com/photos/CtESCAkBfBU).
+
+* Photo by [Benigno Hoyuela](https://unsplash.com/@benignohoyuela) on [Unsplash](https://unsplash.com/photos/2CJIkl-df2M).
+
+* Photo by [Frederick Tubiermont](https://unsplash.com/@callmefred) on [Unsplash](https://unsplash.com/photos/BUAoLq-sOFo).
+
+* Photo by [Daniel von Appen](https://unsplash.com/@daniel_von_appen) on [Unsplash](https://unsplash.com/photos/lP5j4WAlPic).
+
+* Photo by [Toa Heftiba](https://unsplash.com/@heftiba) on [Unsplash](https://unsplash.com/photos/O3ymvT7Wf9U).
+
+### Book Cover Background
+
+Background vector created by [freepik - www.freepik.com](https://www.freepik.com/free-photos-vectors/background).
+
+### Illustrations & Figures
+
+* Photo by [Connor Coyne](https://unsplash.com/@concoyne) on [Unsplash](https://unsplash.com/photos/jG4Fi7yq5k4).
+
+* Photo by [Sung Jin Cho](https://unsplash.com/@mbuff) on [Unsplash](https://unsplash.com/photos/i6-447LjtqY).
+
+* [Photo by Phillip Capper (PhillipC)](https://commons.wikimedia.org/wiki/File:Hawkes_Bay_NZ.jpg), used under [CC BY](https://creativecommons.org/licenses/by/2.0).
+
+* [Photo by Phillip Capper (PhillipC)](https://commons.wikimedia.org/wiki/File:Equalized_Hawkes_Bay_NZ), used under [CC BY](https://creativecommons.org/licenses/by/2.0).
+
+## Icons
+
+A handful of icons in this Playground Book are created by [Icons8](https://icons8.com/).
+
+*/
